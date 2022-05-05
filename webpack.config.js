5b675a09dd4f24d389ee8304ea22e4dd7dd824f7
exile2003 <31631692+exile2003@users.noreply.github.com>
2022-05-05 20:08:18 +0200

    Release-1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: __dirname + '/src/index.js',
@@ -22,6 +23,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            'window.jQuery': 'jquery'
         })
     ],
 }
