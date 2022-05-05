541d16573cfaf02e6cb714370542eeb1a86357eb
exile2003 <31631692+exile2003@users.noreply.github.com>
2022-05-05 20:13:43 +0200

    Release-1.2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import './style.css';
-import '../css/bootstrap.min.css';
+//import '../css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../js/bootstrap-filestyle.min.js';
 
