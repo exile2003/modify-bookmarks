55eb5b72699cec6bdd3f5cdb59e1f4b9888ba8dd
exile2003 <31631692+exile2003@users.noreply.github.com>
2022-05-16 03:33:07 +0200

    Release-1.4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 //const img, img2;
 import './style.css';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../js/bootstrap-filestyle.min.js';
